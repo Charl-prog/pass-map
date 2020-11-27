2eaa81810100244e526ce0639b38253232241da8
Charl-prog <73740273+Charl-prog@users.noreply.github.com>
2020-11-27 11:53:50 +0000

    Deploying to gh-pages from master @ 30fd76515c469c35925b1b9c7f4369004744db20 🚀

--- Tilesets/[A]Water_pipo.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="[A]Water_pipo" tilewidth="32" tileheight="32" tilecount="3072" columns="64">
- <image source="Tilesets/[A]Water_pipo.png" width="2048" height="1536"/>
-</tileset>
--- Tilesets/office_3.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="office_3" tilewidth="32" tileheight="32" tilecount="66" columns="6">
- <image source="Tilesets/office_3.png" width="192" height="352"/>
-</tileset>
